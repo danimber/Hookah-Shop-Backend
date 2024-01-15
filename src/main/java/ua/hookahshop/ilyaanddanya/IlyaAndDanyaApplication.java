fc567b892f02eb0e2f4e8211b2780eacdc8e1bff
danimber <imberbazy@gmail.com>
2024-01-16 00:48:59 +0300

    bug fixed

--- src/main/java/ua/hookahshop/ilyaanddanya/IlyaAndDanyaApplication.java
+++ src/main/java/ua/hookahshop/ilyaanddanya/IlyaAndDanyaApplication.java
@@ -1,8 +1,8 @@
 package ua.hookahshop.ilyaanddanya;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class IlyaAndDanyaApplication {
